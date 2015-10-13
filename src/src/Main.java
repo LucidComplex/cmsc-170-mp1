a711715cc3d811780a4ca578f8f02abc0864c5c3
Marie <mariethegreat89303@gmail.com>
2015-10-13 22:07:10 +0800

    implement file reading

---
+++ src/src/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ *
+ * @author MiriamMarie
+ */
+public class Main {
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        MazeReader mr = new MazeReader();
+        mr.read("tinyMaze.lay.txt");
+    }
+}
---
+++ src/src/MazeReader.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author MiriamMarie
+ */
+public class MazeReader {
+    public MazeReader() {
+        
+    }
+    
+    public char[][] read(String mazeName) throws UnsupportedEncodingException {
+        InputStream is = getClass().getResourceAsStream("/mazes/" + mazeName);
+        BufferedReader br = new BufferedReader(new InputStreamReader(is, "utf-8"));
+        
+        // load maze into list
+        String strLine;
+        List<String> lines = new ArrayList<>();
+        try {
+            while((strLine = br.readLine()) != null){
+                lines.add(strLine);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(MazeReader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        // convert list to array
+        char[][] maze = new char[lines.get(0).length()][lines.size()];
+        for (int i = 0; i < maze.length; i++) {
+            maze[i] = lines.get(i).toCharArray();
+        }
+        System.out.println(Arrays.deepToString(maze));
+        return null;
+    }
+}
