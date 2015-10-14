34c0f4eae0b6f1d87764cae036832d354c0c44b8
faielomente <frlomente@up.edu.ph>
2015-10-15 01:34:03 +0800

    added GreedyBfsMan class

---
+++ src/src/GreedyBfsMan.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src;
+
+/**
+ *
+ * @author parfait
+ */
+public class GreedyBfsMan extends Algorithm{
+    
+    public GreedyBfsMan(char[][] maze, String mazeName){
+        this.init(maze, mazeName);
+    }
+
+    @Override
+    public void solve() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double heuristic(Node node) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
